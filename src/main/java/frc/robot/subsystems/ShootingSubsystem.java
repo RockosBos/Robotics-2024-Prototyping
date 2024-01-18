06017c05fa622e67a1620c4a6576a38ebe94cc18
Lawrence Zhang <lawrencez1110@gmail.com>
2024-01-18 17:06:34 -0500

    Autum Original code

---
+++ src/main/java/frc/robot/subsystems/ShootingSubsystem.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.MotorCommutation;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ShootingSubsystem extends SubsystemBase {
+  /** Creates a new ShootingSubsystem. */
+  public ShootingSubsystem() {}
+
+  private static CANSparkMax motor = new CANSparkMax(Constants.sampleMotorID, MotorType.kBrushless);
+
+  private double speed = 0;
+
+ public ShootingSubsystem(){
+
+ }
+
+ public void setMotorSpeed(double speed){
+  this.speed = speed;
+}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    motor.set(speed);
+    System.out.println(speed);
+  }
+}
